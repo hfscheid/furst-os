91c306ef94724a27eae0722bbf72b86001a48c28
Henrique Fürst <hfscheid@gmail.com>
2025-10-06 15:48:34 -0300

    feat: serial printing for tests

--- src/src/main.rs
+++ src/src/main.rs
@@ -4,10 +4,20 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-
 use core::panic::PanicInfo;
+
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
     loop {}
 }
 
@@ -16,18 +26,42 @@ mod vga_buffer;
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !{
     #[cfg(test)]
-    {
-        test_main();
-        loop {}
-    }
+    test_main();
+
     println!("Hello World! The answer is {}", 42);
     loop {}
 }
 
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed  = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests{
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
+
+#[test_case]
+fn initial_assertion() {
+    serial_print!("initial assertion... ");
+    assert_eq!(1,1);
+    serial_println!("[ok]");
+}
+
+mod serial;
